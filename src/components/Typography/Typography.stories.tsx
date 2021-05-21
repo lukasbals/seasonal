335aac5fb82699a45e32ae702f775e9298923c06
Lukas Bals <mail@lukasbals.at>
2021-05-21 12:08:14 +0100

    Add typographies to storybook

--- src/components/Typography/Typography.stories.tsx
+++ src/components/Typography/Typography.stories.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Meta } from '@storybook/react';
-import { Heading1, SubHeading } from '.';
+import { Heading1, Heading2, SubHeading, Text } from '.';
 
 export default {
   title: 'Typography',
 } as Meta;
 
 export const heading1: React.FC = () => <Heading1>Heading 1</Heading1>;
+export const heading2: React.FC = () => <Heading2>Heading 2</Heading2>;
 export const subHeading: React.FC = () => <SubHeading>Sub heading</SubHeading>;
+export const text: React.FC = () => <Text>Text</Text>;
